c52f7b0c0c4c6b22ab16c37d68ff04fc73c7def3
David <davidli.stuy@gmail.com>
2014-11-09 22:35:19 -0500

    needs testing

--- SuperArray.java
+++ SuperArray.java
@@ -10,30 +10,7 @@ public class SuperArray{
 		data = new Object[n];
 	}
 
-	public String toString(){
-		String str = "[ ";
-		for (int i = 0; i < data.length; i++){
-			str += data[i] + " ";
-		}
-		str += "]";
-		return str;
-	}
-
-	public void add(Object e){
-		for (int x = 0; x < data.length; x++){
-			if (data[x] == null){
-				data[x] = e;
-				break;
-			}
-		}
-	}
-
 	public int size(){
-		for (int x = 0; x < data.length; x++){
-			if (data[x] != null){
-				elements++;
-			}
-		}
 		return elements;
 	}
 
@@ -45,20 +22,83 @@ public class SuperArray{
 		data = data2;
 	}
 
+	private void increaseCap(){
+		resize(data.length + 1);
+	}
+	private void decreaseCap(){
+		resize(data.length - 1);
+	}	
 	public void clear(){
 		for (int i = 0; i < data.length; i++){
 			data[i] = null;
 		}
 	}
+	public String toString(){
+		String str = "[ ";
+		for (int i = 0; i < data.length; i++){
+			str += data[i] + " ";
+		}
+		str += "]";
+		return str;
+	}
+
+	public void add(Object o){
+		add(elements, o);
+	}
+	public void add(int index, Object o){
+		if(index >= elements || index < 0){
+			System.out.println("Out of Bounds.");
+		}
+		if(elements == data.length){
+			increaseCap();
+		}
+		for(int i = elements; i > index; i--){
+			data[i - 1] = data[i];
+		}
+		elements++;
+		data[index] = o;
+	}
 
 	public Object get(int index){
+		if (index < 0  || index >= elements){
+			System.out.println("Out of Bounds.");
+			return null;
+		}
 		return data[index];
+		
+	}
+	
+	public Object set(int index, Object o){
+		if(index >= elements || index < 0){
+			System.out.println("Out of Bounds.");
+			return null;
+		}
+		else{
+			Object ret = data[index];
+			data[index] = o;
+			return ret;
+		}
 	}
 
-	public void set(int index, Object e){
-		data[index] = e;
+	public Object remove(int index){
+		if(index > elements || index < 0){
+			System.out.println("Out of Bounds.");
+			return null;
+		}
+		else{
+			Object ret = data[index];
+			int lastIndex = elements - 1;
+			if(lastIndex < data.length - 1){
+				decreaseCap();
+			}
+			for(int i = index; i < lastIndex; i++){
+				data[i] = data[i + 1];
+			}
+			data[lastIndex] = null;
+			elements--;
+			return ret;
+		}
 	}
-
 	public static void main(String[]args){
 		SuperArray a = new SuperArray();
 		System.out.println(a.toString());
