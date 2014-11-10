60eefbda35b4875712f7be9f11ddded1a9995465
David <davidli.stuy@gmail.com>
2014-11-10 02:06:37 -0500

    superarray2

--- SuperArray.java
+++ SuperArray.java
@@ -22,15 +22,80 @@ public class SuperArray{
 		data = data2;
 	}
 
-	private void increaseCap(){
-		resize(data.length + 1);
-	}
-	private void decreaseCap(){
-		resize(data.length - 1);
-	}	
 	public void clear(){
 		for (int i = 0; i < data.length; i++){
 			data[i] = null;
+		}
+		elements = 0;
+	}
+
+
+	public void add(Object o){
+		add(elements, o);
+	}
+	public void add(int index, Object o){
+		if(index > elements || index < 0){
+			System.out.println("Out of Bounds.");
+		}
+		if(elements == data.length){
+			resize(data.length + 1);
+		}
+		for(int i = elements; i > index;i--){
+			data[i - 1] = data[i];
+		}
+		elements++;
+		data[index] = o;
+		format();
+	}
+
+	public Object get(int index){
+		if (index >= data.length || index < 0){
+			System.out.println("Out of Bounds.");
+			return null;
+		}
+		return data[index];
+		
+	}
+
+	public Object set(int index, Object o){
+		if(index >= elements || index < 0){
+			System.out.println("Out of Bounds.");
+			return null;
+		}
+		else{
+			Object ret = data[index];
+			data[index] = o;
+			format();
+			return ret;
+		}
+	}
+	public Object remove(int index){
+		if(index > elements || index < 0){
+			System.out.println("Out of Bounds.");
+			return null;
+		}
+		else{
+			Object ret = data[index];
+			int lastIndex = elements - 1;
+			if(lastIndex == data.length){
+				resize(data.length - 1);
+			}
+			for(int i = index; i < lastIndex; i++){
+				data[i] = data[i + 1];
+			}
+			data[lastIndex] = null;
+			elements--;
+			format();
+			return ret;
+		}
+	}
+	public void format(){
+		for (int i = 0; i < data.length; i++){
+			if (data[i] == null){
+				data[i] = data[i + 1];
+				data[i + 1] = data[i + 2];
+				i++;
+			}
 		}
 	}
 	public String toString(){
@@ -40,84 +105,25 @@ public class SuperArray{
 		}
 		str += "]";
 		return str;
-	}
-
-	public void add(Object o){
-		add(elements, o);
-	}
-	public void add(int index, Object o){
-		if(index >= elements || index < 0){
-			System.out.println("Out of Bounds.");
-		}
-		if(elements == data.length){
-			increaseCap();
-		}
-		for(int i = elements; i > index; i--){
-			data[i - 1] = data[i];
-		}
-		elements++;
-		data[index] = o;
-	}
-
-	public Object get(int index){
-		if (index < 0  || index >= elements){
-			System.out.println("Out of Bounds.");
-			return null;
-		}
-		return data[index];
-		
-	}
-	
-	public Object set(int index, Object o){
-		if(index >= elements || index < 0){
-			System.out.println("Out of Bounds.");
-			return null;
-		}
-		else{
-			Object ret = data[index];
-			data[index] = o;
-			return ret;
-		}
-	}
-
-	public Object remove(int index){
-		if(index > elements || index < 0){
-			System.out.println("Out of Bounds.");
-			return null;
-		}
-		else{
-			Object ret = data[index];
-			int lastIndex = elements - 1;
-			if(lastIndex < data.length - 1){
-				decreaseCap();
-			}
-			for(int i = index; i < lastIndex; i++){
-				data[i] = data[i + 1];
-			}
-			data[lastIndex] = null;
-			elements--;
-			return ret;
-		}
-	}
+	}	
 	public static void main(String[]args){
 		SuperArray a = new SuperArray();
 		System.out.println(a.toString());
 		System.out.println(a.size());
 		a.add(5);
+		a.resize(20);
+		System.out.println(a.toString());
 		a.add("w");
+		a.add(5);
+		a.add(2,"test");
+		a.add(5);
+		a.add(8,"onetw");
+		a.add(10,7845321);
+		a.add(5);
+		a.add("yay");
 		System.out.println(a.toString());
-		System.out.println(a.size());
-		a.resize(15);
-		System.out.println(a.toString());
-		a.clear();
-		System.out.println(a.toString());
-		a.add(7);
-		a.add("b");
-		a.add(50);
-		a.add("aw");
-		System.out.println(a.toString());
-		System.out.println(a.get(3));
-		a.set(2,"swag");
+		a.remove(9);
+		a.remove(5);
 		System.out.println(a.toString());
 	}
 }
