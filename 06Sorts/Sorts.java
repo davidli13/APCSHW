bc7e2856ae80bbc57378ff254ddb90a5a701acd1
David <davidli.stuy@gmail.com>
2014-12-17 22:59:06 -0500

    commit

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,21 +1,51 @@
 public class Sorts{
-    public static void bubble(int[]c){
-	for (int i = 1; i < c.length; i++){
-	    for (int j = i; j > 1; j--){
-		int x = c[i];
-		if (c[i] < c[i-1]){
-		    c[i] = c[i-1];
-		    c[i-1] = x;
+	public static void Bubble(int[] c){
+		boolean mixed = true;
+		int x;
+		while (mixed){
+			mixed = false;
+			for (int i = 0; i < c.length - 1; i++){
+				if (c[i] > c[i+1]){
+					x = c[i];
+					c[i] = c[i+1];
+					c[i+1] = x;
+					mixed = true;
+				}
+			}
 		}
-	    }	    
 	}
-    }
-    
-    public static void main(String[]args){
-	int[]c = new int[10];
-	for (int i = 0; i < 10; i++){
-	    c[i] = i;
+
+	public static void InsertionSort(int[] c){
+		int x, h; 
+		for(int i = 0; i < c.length; i++){
+			x = i;
+			while (x > 0 && c[x] < c[x-1]){
+				h = c[x-1];
+				c[x-1] = c[x];
+				c[x] = h;
+				x--;
+			}
+		}
 	}
-	Sorts.bubble(c);
-    }
+
+	public static void SelectionSort (int[] c){
+		int x, min, h;
+		for(int i = 0; i < c.length; i++){
+			min = c[i];
+			h = i;
+			for(int j = i; j < c.length; j++){
+				if(min >= c[j]){
+					min = c[j];
+					h = j;
+				}
+			}
+			x = c[i];
+			c[i] = min;
+			c[h] = x;
+		}
+	}
+
+	public static void main(String[]args){
+
+	}
 }
